8ba8f670401c68f4849fde06e6c2aaa0679b839b
fedechka1245 <bragin.fedor96@gmail.com>
2022-10-06 02:21:18 +0500

    fix sheet parsing

--- src/helpers/parseGradesTimetable.js
+++ src/helpers/parseGradesTimetable.js
@@ -1,6 +1,10 @@
 import {readFile} from "./readFile";
 import Excel from "exceljs";
 
+const getCellText = (cell) => {
+  return cell.value !== null ? cell.text : '-'
+}
+
 export const parseGradesTimetable = async (file) => {
   const buffer = await readFile(file)
   const workbook = new Excel.Workbook()
@@ -11,22 +15,26 @@ export const parseGradesTimetable = async (file) => {
 
   for (let row = 1; row <= sheet.rowCount; row++) {
     for (let column = 1; column <= sheet.columnCount; column++) {
-      if (!sheet.getCell(row, column)?.text) {
-        return;
-      }
+      try {
+        const cellValue = getCellText(sheet.getCell(row, column))
 
-      const cellValue = sheet.getCell(row, column)?.text
+        if (grades.find((grade) => grade.name === cellValue)) {
+          continue
+        }
 
-      if (typeof cellValue === "string" && cellValue?.toLowerCase()?.includes(' класс') && cellValue !== sheet.getCell(row, column - 1)?.text) {
-        grades.push({
-          name: cellValue,
-          timetable: Array.from({ length: 7 }).map((_, index) => (
-            {
-              subject: sheet.getCell(row + index + 1, column)?.text,
-              classRoom: sheet.getCell(row + index + 1, column + 1)?.text,
-            }
-          ))
-        })
+        if (cellValue?.toLowerCase()?.includes(' класс')) {
+          grades.push({
+            name: cellValue,
+            timetable: Array.from({ length: 7 }).map((_, index) => {
+              return {
+                subject: getCellText(sheet.getCell(row + index + 1, column)),
+                classRoom: getCellText(sheet.getCell(row + index + 1, column + 1)),
+              }
+            })
+          })
+        }
+      } catch {
+        continue
       }
     }
   }
